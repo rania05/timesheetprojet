0c62bb0bb2403f20da343348bd889f9d0c86fbec
warmaster05 <rania.arfaoui.1@esprit.tn>
2021-11-16 23:22:26 +0000

    configuration

---
+++ Desktop/devops-anis/timesheet/src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -0,0 +1,19 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+
+public interface IEntrepriseService {
+	
+	public int ajouterEntreprise(Entreprise entreprise);
+	public int ajouterDepartement(Departement dep);
+	void affecterDepartementAEntreprise(int depId, int entrepriseId);
+	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
+	public void deleteEntrepriseById(int entrepriseId);
+	public void deleteDepartementById(int depId);
+	public Entreprise getEntrepriseById(int entrepriseId);
+	public Optional<Entreprise> optionalGetEntrepriseById(int entrepriseId);
+}
---
+++ Desktop/devops-anis/timesheet/src/test/java/tn/esprit/spring/EmployeTest.java
@@ -0,0 +1,76 @@
+package tn.esprit.spring;
+
+
+
+
+import java.text.ParseException;
+
+
+import java.util.Optional;
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.IEmployeService;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(AnnotationAwareAspectJAutoProxyCreator.class)
+public class EmployeTest {
+	//@Autowired
+	//private EmployeServiceImpl e ;
+	
+	@Autowired
+	IEmployeService e;
+
+	
+
+	
+	private Employe emp = new Employe("Test", "Test", "Test@test.tn", "root", true, Role.INGENIEUR);
+	
+	@Test
+	public void testAdd()
+	{
+		
+		
+		int res = e.addOrUpdateEmploye(emp);
+		assertFalse("Error in Adding Employe", res==0);
+		assertTrue("Added Emplye Successfully",res!=0);	
+	}
+	
+	@Test
+	public void testGetEmployePrenomById()
+	{
+		
+		
+		int id = e.addOrUpdateEmploye(emp);
+		String res = e.getEmployePrenomById(id);
+		assertFalse("Error in getting Employe Prenom", res==null);
+		assertTrue("Got Employe Prenom successfully",res!=null);
+	}
+	
+	@Test
+	public void testRemoveEmploye()
+	{
+		e.deleteEmployeById(11);
+		String res = e.getEmployePrenomById(11);
+		System.out.println(res);
+		assertThat(res==null);
+	}
+
+}
